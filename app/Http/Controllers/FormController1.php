81a3b6e02a48a6986ce23ce6988ec29eb289e58f
kmjak <tymkoron1221@icloud.com>
2024-12-18 15:00:57 +0900

    commit

---
+++ app/Http/Controllers/FormController1.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormController1 extends Controller
+{
+    public function index(Request $req)
+    {
+        $params = [
+            'method' => $req->method(),
+            'val1' => $req->input('val1', null),
+            'val2' => $req->input('val2', null),
+            'msg' => $req->input('msg', null),
+            'item' => ['apple', 'banana', 'cherry'],
+        ];
+        return view('assignment/form1', $params);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\info;
 // use App\Http\Controllers\info_user;
 use App\Http\Controllers\req_res;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController1;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,3 +26,6 @@ Route::post('/abe', [AbeController::class,'index'])->name('abe');
 Route::get('/directive', function () {
     return view('directive');
 })->name('directive');
+
+Route::get('/assignment/1', [FormController1::class, 'index'])->name('assignment1');
+Route::post('/assignment/1', [FormController1::class, 'index'])->name('assignment1');
